69feb93e605a4af7b5333a0cce26f136eb9b5731
rsbresny <richardsbresny@gmail.com>
2017-01-04 20:05:26 -0500

    Added contact feedback

--- js/contact_me.js
+++ js/contact_me.js
@@ -27,6 +27,17 @@ $(function() {
                     message: message
                 },
                 dataType: 'json',
+                beforeSend: function(){
+                  $contactForm.append('<div class = "alert alert--loading">Sending message...</div>');
+                },
+                success: function(data) {
+                  $contactForm.find('alert--loading').hide();
+                  $contactForm.append('<div class = "alert alert--success">Message sent!</div>');
+                },
+                error: function(err) {
+                  $contactForm.find('alert--loading').hide();
+                  $contactForm.append('<div class = "alert alert--error">Oops, there was an error.</div>');
+                }
             })
         },
         filter: function() {
